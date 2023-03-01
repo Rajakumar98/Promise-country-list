6467a7383f59317a3bb6e3509b9456d223210823
Rajakumar98 <rajakumar1998mech@gmail.com>
2023-03-01 14:50:20 +0530

    secondcommit

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const root = document.getElementById("root");
-
 const allItems = fetch("https://restcountries.com/v3.1/all");
-
 const list = document.createElement("ol");
 allItems
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
     const newData = data
-      //   .filter((item) => {
-      //     return item.name.common === "Armenia";
-      //   })
       .map((item) => {
         const li = document.createElement("li");
         const link = document.createElement("a");
